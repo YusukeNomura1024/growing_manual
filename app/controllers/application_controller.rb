cccec3b613d02b048a0f7de12e3211566e5a47ba
YusukeNomura1024 <joblesser@gmail.com>
2021-09-08 03:34:07 +0000

    [Add] function__user__sessions,registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    case resource
+    when AdminUser
+      admin_users_home_path
+    when User
+      '/'
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin_user
+      new_admin_user_session_path
+    when :user
+      new_user_session_path
+    end
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    case resource
+    when :admin_user
+      admin_users_home_path
+    when :user
+      '/'
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  scope module: :public do
+    root "homes#top"
+    get "about", to: 'homes#about'
+  end
+
+  devise_for :users, controllers: {
+      sessions: 'public/sessions',
+      registrations: 'public/registrations',
+      passwords: 'public/passwords'
+    }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
