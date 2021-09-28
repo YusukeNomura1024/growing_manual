8b577fe112d6ca664a8d0c11241f5742245a37aa
YusukeNomura1024 <joblesser@gmail.com>
2021-09-28 15:54:46 +0000

    [Add] function__procedures_controller_authority_management

--- app/assets/javascripts/public/manuals.coffee
+++ app/assets/javascripts/public/manuals.coffee
@@ -1,8 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-
-$ ->
+window.document.addEventListener('turbolinks:load', () ->
   el = document.getElementById("sortable_list")
   if el != null
     sortable = Sortable.create(el,
@@ -12,5 +11,5 @@ $ ->
           type: 'patch'
           data: { from: evt.oldIndex, to: evt.newIndex }
     )
-    
-    
+
+);
--- app/controllers/public/procedures_controller.rb
+++ app/controllers/public/procedures_controller.rb
@@ -1,43 +1,58 @@
 class Public::ProceduresController < ApplicationController
   before_action :authenticate_user!, except: [:top, :about]
-  
+
   def index
     @manual = Manual.find(params[:manual_id])
+    non_manual_owner_to_root
     @procedures = @manual.procedures
   end
 
   def create
-
     procedure = Procedure.new(procedure_params)
-    procedure.manual_id = params[:manual_id]
-    max_position = Procedure.where(manual_id: params[:manual_id]).maximum(:position)
+    @manual = Manual.find(params[:manual_id])
+    procedure.manual_id = @manual.id
+    non_manual_owner_to_root
+    max_position = Procedure.where(manual_id: @manual.id).maximum(:position)
+
+    # 一度もprocedureを作成していない場合の処理
     if max_position.nil?
       procedure.position = 1
-
     end
     procedure.save
-    redirect_to manual_procedures_path(params[:manual_id])
+    redirect_to manual_procedures_path(@manual)
   end
 
   def destroy
     procedure = Procedure.find(params[:id])
+    @manual = Manual.find(params[:manual_id])
+    non_manual_owner_to_root
     procedure.destroy
     redirect_to manual_procedures_path(params[:manual_id])
   end
 
   def edit
-    @procedure = Procedure.find(params[:id])
     @manual = Manual.find(params[:manual_id])
+    non_manual_owner_to_root
+    @procedure = Procedure.find(params[:id])
 
   end
 
   def update
+    @manual = Manual.find(params[:manual_id])
+    non_manual_owner_to_root
     procedure = Procedure.find(params[:id])
     procedure.update(procedure_params)
     redirect_to manual_procedures_path(procedure.manual.id)
   end
 
+
   private
+
+  def non_manual_owner_to_root
+    if @manual.user != current_user
+      redirect_to '/'
+    end
+  end
 
   def procedure_params
     params.require(:procedure).permit(:title, :position)
