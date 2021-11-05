a968d271dc7cbd96c6f9d4b45f5632dd929b8c16
YusukeNomura1024 <joblesser@gmail.com>
2021-11-05 15:37:15 +0000

    [Fix] migrate create_bookmarks.rb

--- db/migrate/20210908034109_create_bookmarks.rb
+++ db/migrate/20210908034109_create_bookmarks.rb
@@ -1,8 +1,8 @@
 class CreateBookmarks < ActiveRecord::Migration[5.2]
   def change
     create_table :bookmarks do |t|
-      t.references :user, type: :bigint, foreign_key: true, null: false
-      t.references :manual, type: :bigint, foreign_key: true, null: false
+      t.references :user, type: :integer, foreign_key: true, null: false
+      t.references :manual, type: :integer, foreign_key: true, null: false
 
       t.timestamps
     end
