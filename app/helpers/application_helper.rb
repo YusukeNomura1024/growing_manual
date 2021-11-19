89c1a9c40060ddd9ca5f854ed19d693fd3cee200
YusukeNomura1024 <joblesser@gmail.com>
2021-11-19 06:41:48 +0000

    [Fix] design about_page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,47 @@ module ApplicationHelper
   require "redcarpet"
   require "coderay"
 
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+    def block_code(code, language)
+      language = language.split(':')[0]
+
+      case language.to_s
+      when 'rb'
+        lang = "ruby"
+      when 'yml'
+        lang = 'yaml'
+      when 'css'
+        lang = 'css'
+      when 'html'
+        lang = 'html'
+      when ''
+        lang = 'md'
+      else
+        lang = language
+      end
+
+      CodeRay.scan(code, lang).div
+    end
+  end
+
+  def markdown(text)
+    html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      tables: true,
+      hard_wrap: true,
+      xhtml: true,
+      lax_html_blocks: true,
+      strikethrough: true
+    }
+    markdown = Redcarpet::Markdown.new(html_render, options)
+
+    markdown.render(text).html_safe
+  end
+
   def text_url_to_link(text)
     URI.extract(text, ['http', 'https']).uniq.each do |url|
       sub_text = ""
@@ -14,5 +55,5 @@ module ApplicationHelper
     return text
   end
 
-  
+
 end
