cfe47d8642fab65c159b329d6c42a1f92e5a9116
YusukeNomura1024 <joblesser@gmail.com>
2021-11-21 14:46:38 +0000

    [Add] design animation slideIn_text slideIn_img

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,10 @@
-// スクロールすると文字が表示される
+// スクロールすると文字や画像が表示される
 window.document.addEventListener('turbolinks:load', function(){
   const backTextShow = function(){
     const innerTitleBackText = document.getElementsByClassName('inner__title_back_text');
+    const innerImg = document.getElementsByClassName('inner__img');
+    const innerDirection = document.getElementsByClassName('inner__direction');
+    
 
     const position = Math.floor(window.innerHeight * .75);
 
@@ -9,6 +12,18 @@ window.document.addEventListener('turbolinks:load', function(){
       let offsetTop = Math.floor(innerTitleBackText[i].getBoundingClientRect().top);
       if (offsetTop < position) {
         innerTitleBackText[i].classList.add('text_move--slidein_y');
+      }
+    }
+    for (let i = 0; i < innerImg.length; i++) {
+      let offsetTop = Math.floor(innerImg[i].getBoundingClientRect().top);
+      if (offsetTop < position) {
+        innerImg[i].classList.add('img_move--slidein_x');
+      }
+    }
+    for (let i = 0; i < innerDirection.length; i++) {
+      let offsetTop = Math.floor(innerDirection[i].getBoundingClientRect().top);
+      if (offsetTop < position) {
+        innerDirection[i].classList.add('text_move--slidein_reverse_x');
       }
     }
   };
