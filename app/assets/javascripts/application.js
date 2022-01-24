6b49d8b24c2987494ec1426b23ca81074ce901cb
YusukeNomura1024 <joblesser@gmail.com>
2022-01-24 11:37:46 +0000

    [Add] swiperJsの導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,5 +72,22 @@ window.document.addEventListener('turbolinks:load', function(){
     });
   };
 
+  const swiper = new Swiper('.swiper', {
+    // direction: 'vertical',
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 })
 
