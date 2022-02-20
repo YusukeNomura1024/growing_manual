ae591b1cf7b161f8529c69f9e90346c2449bd1fa
YusukeNomura1024 <joblesser@gmail.com>
2022-02-20 14:12:02 +0000

    [Fix] config force_ssl

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,22 +72,5 @@ window.document.addEventListener('turbolinks:load', function(){
     });
   };
 
-  const swiper = new Swiper('.swiper', {
-    // direction: 'vertical',
-    loop: true,
-
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
 })
 
